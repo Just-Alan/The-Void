3d7a3d66bdd40ce966c8195575298de7e059db4a
Alan Sebas <pan123xdxd@gmail.com>
2026-06-29 22:31:43 -0500

    fixed minor bug with excnge.js

--- api/exchange.js
+++ api/exchange.js
@@ -1,14 +1,14 @@
 export default async function handler(req, res) {
     const { code } = req.body;
 
-    const response = await fetch("https://the-void-dusky.vercel.app/api/exchange", {
+    const response = await fetch("https://auth.hackclub.com/oauth/token", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
             client_id: process.env.HCA_CLIENT_ID,
             client_secret: process.env.HCA_CLIENT_SECRET,
             code: code,
-            redirect_uri: "https://the-void-dusky.vercel.app/auth/callback.html",  
+            redirect_uri: "https://the-void-dusky.vercel.app/auth/callback.html",
             grant_type: "authorization_code"
         })
     });
